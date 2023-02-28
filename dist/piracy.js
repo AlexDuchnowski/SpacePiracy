0e05fc819d57136cf729bcaeb93604c9c9c156b4
AlexDuchnowski <alex_duchnowski@brown.edu>
2023-02-28 16:46:15 -0500

    added input validation and improved aesthetics of interaction

--- dist/piracy.js
+++ dist/piracy.js
@@ -4,18 +4,23 @@ let answers = hexes.map(h => parseInt(h, 16));
 console.log("Answer numbers:");
 answers.forEach(a => console.log(a));
 let container = document.getElementById('response-container');
+let inputBox = document.getElementById('guess');
 let response = document.createElement('a');
 let defaultMessage = "Type a number and press Enter to make a guess.";
 response.innerText = defaultMessage;
 container.appendChild(response);
 function processGuess() {
-    let guess = document.getElementById('guess').value;
+    let guess = inputBox.value;
     guess = guess.toUpperCase();
     // console.log('Guess:' + guess)
-    if (guess) {
+    if (!guess) {
+        response.innerText = defaultMessage;
+    }
+    else if (guess.match('(^([A-F]|[0-9])+$)')) {
         response.innerText = guess;
     }
     else {
-        response.innerText = defaultMessage;
+        response.innerText = guess + " isn't a number I recognize.";
     }
+    inputBox.value = '';
 }
--- src/piracy.ts
+++ src/piracy.ts
@@ -4,18 +4,22 @@ console.log("Answer numbers:")
 answers.forEach(a => console.log(a))
 
 let container = document.getElementById('response-container') as HTMLElement
+let inputBox = (document.getElementById('guess') as HTMLInputElement)
 let response = document.createElement('a')
 let defaultMessage = "Type a number and press Enter to make a guess."
 response.innerText = defaultMessage
 container.appendChild(response)
 
 function processGuess() {
-    let guess = (document.getElementById('guess') as HTMLInputElement).value
+    let guess = inputBox.value
     guess = guess.toUpperCase()
     // console.log('Guess:' + guess)
-    if (guess) {
+    if (!guess) {
+        response.innerText = defaultMessage
+    } else if (guess.match('(^([A-F]|[0-9])+$)')) {
         response.innerText = guess
     } else {
-        response.innerText = defaultMessage
+        response.innerText = guess + " isn't a number I recognize."
     }
+    inputBox.value = ''
 }
