3b1d9502b5a7fb8a29978c92e839dbf2be57c7eb
AlexDuchnowski <alex_duchnowski@brown.edu>
2023-02-28 17:37:54 -0500

    added feedback table

--- src/piracy.ts
+++ src/piracy.ts
@@ -1,7 +1,5 @@
 let hexes = ['51', '500', 'face']
 let answers = hexes.map(h => parseInt(h, 16))
-console.log("Answer numbers:")
-answers.forEach(a => console.log(a))
 
 let container = document.getElementById('response-container') as HTMLElement
 let inputBox = (document.getElementById('guess') as HTMLInputElement)
@@ -10,6 +8,26 @@ let defaultMessage = "Type a number and press Enter to make a guess."
 response.innerText = defaultMessage
 container.appendChild(response)
 
+function guessResults(guess: string): string {
+    let guessVal = parseInt(guess, 16)
+    let hline = "+" + "-".repeat(4) + "+" + "-".repeat(20) + "+\n"
+    let table = "Results for " + guess + "\n" + hline
+    for (let i = 0; i < answers.length; i++) {
+        let answer = answers[i]
+        let row = "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |"
+        if (guessVal < answer) {
+            row += " too small" + "\xa0".repeat(10)
+        } else if (guessVal == answer) {
+            row += " just right!" + "\xa0".repeat(8)
+        } else {
+            row += " too big" + "\xa0".repeat(12)
+        }
+        row += "|\n"
+        table += row + hline
+    }
+    return table
+}
+
 function processGuess() {
     let guess = inputBox.value
     guess = guess.toUpperCase()
@@ -17,9 +35,9 @@ function processGuess() {
     if (!guess) {
         response.innerText = defaultMessage
     } else if (guess.match('(^([A-F]|[0-9])+$)')) {
-        response.innerText = guess
+        response.innerText = guessResults(guess)
     } else {
         response.innerText = guess + " isn't a number I recognize."
     }
-    inputBox.value = ''
+    // inputBox.value = '' // This clears the input field upon completion.
 }
