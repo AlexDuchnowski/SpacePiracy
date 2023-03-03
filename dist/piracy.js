c55f7d1ba069b6adcccb196ea37b3ca97a245f5f
AlexDuchnowski <alex_duchnowski@brown.edu>
2023-03-03 15:47:41 -0500

    first draft

--- dist/piracy.js
+++ dist/piracy.js
@@ -1,20 +1,19 @@
 "use strict";
 let hexes = [
-    'FD0',
-    'C0F1DE',
-    'ABD1CAE',
-    'DCADE',
-    '51',
-    'C0FFE',
-    'BED1DE',
-    '500',
-    'AFF0D',
-    'D15E5E',
-    'DEBAED',
-    'BA5D'
+    'DEC1DE',
+    'FECE5',
+    'FEE',
+    'CA5E',
+    'FACE',
+    'C0D',
+    '50',
+    '15',
+    'DEAD',
+    '51DE',
+    '5AFE5',
+    'BA55'
 ];
 let answers = hexes.map(h => parseInt(h, 16)).sort((n1, n2) => n1 - n2);
-console.log(answers);
 let container = document.getElementById('response-container');
 let inputBox = document.getElementById('guess');
 let response = document.createElement('a');
@@ -25,6 +24,7 @@ function guessResults(guess) {
     let guessVal = parseInt(guess, 16);
     let hline = "+" + "-".repeat(4) + "+" + "-".repeat(14) + "+\n";
     let table = "Results for " + guess + "\n" + hline;
+    let hit = false;
     for (let i = 0; i < answers.length; i++) {
         let answer = answers[i];
         let row = "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |";
@@ -33,6 +33,7 @@ function guessResults(guess) {
         }
         else if (guessVal == answer) {
             row += " just right!" + "\xa0".repeat(2);
+            hit = true;
         }
         else {
             row += " too big" + "\xa0".repeat(6);
@@ -40,17 +41,22 @@ function guessResults(guess) {
         row += "|\n";
         table += row + hline;
     }
-    return table;
+    return [table, hit];
 }
 function processGuess() {
     let guess = inputBox.value;
     guess = guess.toUpperCase();
     // console.log('Guess:' + guess)
+    response.style.color = "cadetblue";
     if (!guess) {
         response.innerText = defaultMessage;
     }
     else if (guess.match('(^([A-F]|[0-9])+$)')) {
-        response.innerText = guessResults(guess);
+        let [table, hit] = guessResults(guess);
+        response.innerText = table;
+        if (hit) {
+            response.style.color = "green";
+        }
     }
     else {
         response.innerText = guess + " isn't a number I recognize.";
