5db2d1f901ac8a2f5739ad5ce2335cb980770637
AlexDuchnowski <alex_duchnowski@brown.edu>
2023-04-02 17:56:59 -0400

    most of second draft

--- dist/piracy.js
+++ dist/piracy.js
@@ -1,62 +1,124 @@
 "use strict";
-let hexes = [
-    'DEC1DE',
-    'FECE5',
-    'FEE',
-    'CA5E',
-    'FACE',
-    'C0D',
-    '50',
-    '15',
-    'DEAD',
-    '51DE',
-    '5AFE5',
-    'BA55'
+let passwords = [
+    'A5C1C0DE',
+    '0B51D1AS',
+    'D1DAC1C5',
+    '60DD55E5',
+    'F0CEFEED',
+    'AC1D1F1D',
+    'AB5CE5E5',
+    'DAABA5E5',
+    '5AC1F1CE',
+    'B51C1DEA',
+    'A550C1AE',
+    '6E0D51C5',
 ];
-let answers = hexes.map(h => parseInt(h, 16)).sort((n1, n2) => n1 - n2);
 let container = document.getElementById('response-container');
 let inputBox = document.getElementById('guess');
-let response = document.createElement('a');
+let errorColWidth = 60;
+let response = document.createElement('p');
 let defaultMessage = "Type a number and press Enter to make a guess.";
 response.innerText = defaultMessage;
 container.appendChild(response);
-function guessResults(guess) {
-    let guessVal = parseInt(guess, 16);
-    let hline = "+" + "-".repeat(4) + "+" + "-".repeat(14) + "+\n";
-    let table = "Results for " + guess + "\n" + hline;
-    let hit = false;
-    for (let i = 0; i < answers.length; i++) {
-        let answer = answers[i];
-        let row = "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |";
-        if (guessVal < answer) {
-            row += " too small" + "\xa0".repeat(4);
+function hexFunctionMachine(f) {
+    /**
+     * Creates and returns a function which applies f to hex strings and returns
+     * a string of the result, throwing an error if the result is not an integer.
+     */
+    return function (hex) {
+        let result = f(parseInt(hex, 16));
+        if (result % 1 != 0) {
+            let message = " Error: Transformation produced a non-integer result.";
+            throw new Error(message + "\xa0".repeat(errorColWidth - message.length));
         }
-        else if (guessVal == answer) {
-            row += " just right!" + "\xa0".repeat(2);
-            hit = true;
+        return result.toString(16).toUpperCase();
+    };
+}
+function reverseString(s) {
+    if (s === "") {
+        return "";
+    }
+    else {
+        return reverseString(s.substring(1)) + s.charAt(0);
+    }
+}
+let transformations = [
+    hexFunctionMachine((n) => n * 2),
+    reverseString,
+    hexFunctionMachine((n) => n / 3),
+    hexFunctionMachine((n) => n / 5),
+    reverseString,
+    reverseString,
+    reverseString,
+    reverseString,
+    reverseString,
+    reverseString,
+    reverseString,
+    reverseString,
+];
+function passwordGuessOverlap(password, guess) {
+    /**
+     * Computes and returns the strings to display correctly
+     * guessed letters in a given password.
+     */
+    let result = "";
+    let remainder = "";
+    let limit = (guess.length < 8) ? guess.length : 8;
+    for (let i = 0; i < limit; i++) {
+        if (password[i] === guess[i]) {
+            result += password[i];
         }
         else {
-            row += " too big" + "\xa0".repeat(6);
+            result += "•";
         }
-        row += "|\n";
-        table += row + hline;
     }
-    return [table, hit];
+    if (guess.length < 8) {
+        remainder += "•".repeat(8 - guess.length);
+    }
+    return [result, remainder];
+}
+function displayResults(guess) {
+    /**
+     * Takes the response element and a guessed number (in string form) and
+     * updates the element to display and whether there was an exact match.
+     */
+    let hline = "+" + "-".repeat(4) + "+" + "-".repeat(errorColWidth) + "+" + "-".repeat(8) + "+<br>";
+    response.innerHTML += "Results for " + guess + "<br>" + hline;
+    for (let i = 0; i < passwords.length; i++) {
+        response.innerHTML += "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |";
+        try {
+            let transformed = transformations[i](guess);
+            if (transformed.length != 8) {
+                let message = " Warning: Result has length " + transformed.length.toString(16).toUpperCase() + ".";
+                response.innerHTML += message + "\xa0".repeat(errorColWidth - message.length) + "|";
+            }
+            else {
+                response.innerHTML += " No errors or warnings :)" + "\xa0".repeat(errorColWidth - 25) + "|";
+            }
+            let [overlap, remainder] = passwordGuessOverlap(passwords[i], transformed);
+            console.log(i, overlap, remainder);
+            response.innerHTML += '<b><u style="color: darkred;">' + overlap + '</u></b>';
+            response.innerHTML += remainder;
+        }
+        catch (error) {
+            response.innerHTML += error.message + "|" + "█".repeat(8);
+        }
+        response.innerHTML += "|<br>" + hline;
+    }
 }
 function processGuess() {
+    /**
+     * A function that is called every time a number is guessed.
+     */
     let guess = inputBox.value;
     guess = guess.toUpperCase();
-    // console.log('Guess:' + guess)
+    response.textContent = "";
     response.style.color = "cadetblue";
     if (!guess) {
         response.innerText = defaultMessage;
     }
     else if (guess.match('(^([A-F]|[0-9])+$)')) {
-        let [table, hit] = guessResults(guess);
-        response.innerText = table;
-        if (hit) {
-            response.style.color = "green";
-        }
+        displayResults(guess);
     }
     else {
         response.innerText = guess + " isn't a number I recognize.";
