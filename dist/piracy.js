bcd3de9e8505dab84a54725ed3a59d05ad402bfb
AlexDuchnowski <alex_duchnowski@brown.edu>
2023-04-03 15:12:09 -0400

    almost done with transformations

--- dist/piracy.js
+++ dist/piracy.js
@@ -16,8 +16,8 @@ let passwords = [
 let container = document.getElementById('response-container');
 let inputBox = document.getElementById('guess');
 let errorColWidth = 60;
-let response = document.createElement('p');
-let defaultMessage = "Type a number and press Enter to make a guess.";
+let response = document.createElement('a');
+let defaultMessage = "Type a number in the field above and press Enter to make a guess.";
 response.innerText = defaultMessage;
 container.appendChild(response);
 function hexFunctionMachine(f) {
@@ -28,11 +28,16 @@ function hexFunctionMachine(f) {
     return function (hex) {
         let result = f(parseInt(hex, 16));
         if (result % 1 != 0) {
-            let message = " Error: Transformation produced a non-integer result.";
-            throw new Error(message + "\xa0".repeat(errorColWidth - message.length));
+            throw new Error(" ERROR: Transformation produced a non-integer result.");
+        }
+        else if (result < 0) {
+            throw new Error(" ERROR: Transformation produced a negative result.");
         }
         return result.toString(16).toUpperCase();
     };
+}
+function simplifyHexString(hex) {
+    return parseInt(hex, 16).toString(16).toUpperCase();
 }
 function reverseString(s) {
     if (s === "") {
@@ -42,19 +47,27 @@ function reverseString(s) {
         return reverseString(s.substring(1)) + s.charAt(0);
     }
 }
+function peelString(s) {
+    if (s.length <= 1) {
+        return s;
+    }
+    else {
+        return s.charAt(0) + s.charAt(s.length - 1) + peelString(s.slice(1, -1));
+    }
+}
 let transformations = [
     hexFunctionMachine((n) => n * 2),
-    reverseString,
+    (hex) => reverseString(simplifyHexString(hex)),
     hexFunctionMachine((n) => n / 3),
     hexFunctionMachine((n) => n / 5),
-    reverseString,
+    hexFunctionMachine((n) => n + parseInt("11111111", 16)),
     hexFunctionMachine((n) => n * 3),
+    hexFunctionMachine((n) => n - parseInt("22222222", 16)),
+    (hex) => peelString(simplifyHexString(hex)),
     reverseString,
-    reverseString,
-    reverseString,
-    reverseString,
+    hexFunctionMachine((n) => parseInt("FFFFFFFF", 16) - n),
     hexFunctionMachine((n) => Math.sqrt(n)),
-    reverseString,
+    hexFunctionMachine((n) => n + parseInt("12345678", 16)),
 ];
 function passwordGuessOverlap(password, guess) {
     /**
@@ -83,13 +96,14 @@ function displayResults(guess) {
      * updates the element to display and whether there was an exact match.
      */
     let hline = "+" + "-".repeat(4) + "+" + "-".repeat(errorColWidth) + "+" + "-".repeat(8) + "+<br>";
-    response.innerHTML += "Results for " + guess + "<br>" + hline;
+    response.innerHTML += "Results for " + guess + "<br>" + hline + "|" + "\xa0".repeat(4) + "|" + " Errors/Warnings" + "\xa0".repeat(errorColWidth - 16) + "| Result |<br>" + hline;
     for (let i = 0; i < passwords.length; i++) {
         response.innerHTML += "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |";
         try {
             let transformed = transformations[i](guess);
+            // console.log(i + 1, transformed)
             if (transformed.length != 8) {
-                let message = " Warning: Result has length " + transformed.length.toString(16).toUpperCase() + ".";
+                let message = " WARNING: Result has length " + transformed.length.toString(16).toUpperCase() + ".";
                 response.innerHTML += message + "\xa0".repeat(errorColWidth - message.length) + "|";
             }
             else {
@@ -97,15 +111,16 @@ function displayResults(guess) {
             }
             let [overlap, remainder] = passwordGuessOverlap(passwords[i], transformed);
             if (!overlap.includes("•") && remainder === "") {
-                response.innerHTML += '<b><u style="color: green;">' + overlap + '</u></b>';
+                response.innerHTML += "<b><u style='color: green;'>" + overlap + '</u></b>';
             }
             else {
-                response.innerHTML += '<b><u style="color: darkred;">' + overlap + '</u></b>';
+                response.innerHTML += "<b><u style='color: orange;'>" + overlap + '</u></b>';
                 response.innerHTML += remainder;
             }
         }
         catch (error) {
-            response.innerHTML += error.message + "|" + "█".repeat(8);
+            let m = error.message;
+            response.innerHTML += m + "\xa0".repeat(errorColWidth - m.length) + "|<span style='color: darkred;'>" + "█".repeat(8) + "</span>";
         }
         response.innerHTML += "|<br>" + hline;
     }
@@ -125,7 +140,7 @@ function processGuess() {
         displayResults(guess);
     }
     else {
-        response.innerText = guess + " isn't a number I recognize.";
+        response.innerText = guess + " isn't recognized as a number. Please ensure that there are no punctuation symbols or other irrelevant characters in your entry.";
     }
     // inputBox.value = '' // This clears the input field upon completion.
 }
--- src/piracy.ts
+++ src/piracy.ts
@@ -16,8 +16,8 @@ let passwords = [
 let container = document.getElementById('response-container') as HTMLElement
 let inputBox = document.getElementById('guess') as HTMLInputElement
 let errorColWidth = 60
-let response = document.createElement('p')
-let defaultMessage = "Type a number and press Enter to make a guess."
+let response = document.createElement('a')
+let defaultMessage = "Type a number in the field above and press Enter to make a guess."
 response.innerText = defaultMessage
 container.appendChild(response)
 
@@ -29,11 +29,16 @@ function hexFunctionMachine(f: ((n: number) => number)): (hex: string) => string
     return function (hex: string): string {
         let result = f(parseInt(hex, 16))
         if (result % 1 != 0) {
-            let message = " Error: Transformation produced a non-integer result."
-            throw new Error(message + "\xa0".repeat(errorColWidth - message.length))
+            throw new Error(" ERROR: Transformation produced a non-integer result.")
+        } else if (result < 0) {
+            throw new Error(" ERROR: Transformation produced a negative result.")
         }
         return result.toString(16).toUpperCase()
     }
+}
+
+function simplifyHexString(hex: string): string {
+    return parseInt(hex, 16).toString(16).toUpperCase()
 }
 
 function reverseString(s: string): string {
@@ -44,19 +49,27 @@ function reverseString(s: string): string {
     }
 }
 
+function peelString(s: string): string {
+    if (s.length <= 1) {
+        return s
+    } else {
+        return s.charAt(0) + s.charAt(s.length - 1) + peelString(s.slice(1, -1))
+    }
+}
+
 let transformations = [
     hexFunctionMachine((n: number) => n * 2),
-    reverseString,
+    (hex: string) => reverseString(simplifyHexString(hex)),
     hexFunctionMachine((n: number) => n / 3),
     hexFunctionMachine((n: number) => n / 5),
-    reverseString,
+    hexFunctionMachine((n: number) => n + parseInt("11111111", 16)),
     hexFunctionMachine((n: number) => n * 3),
+    hexFunctionMachine((n: number) => n - parseInt("22222222", 16)),
+    (hex: string) => peelString(simplifyHexString(hex)),
     reverseString,
-    reverseString,
-    reverseString,
-    reverseString,
+    hexFunctionMachine((n: number) => parseInt("FFFFFFFF", 16) - n),
     hexFunctionMachine((n: number) => Math.sqrt(n)),
-    reverseString,
+    hexFunctionMachine((n: number) => n + parseInt("12345678", 16)),
 ]
 
 function passwordGuessOverlap(password: string, guess: string): [string, string] {
@@ -86,26 +99,28 @@ function displayResults(guess: string): void {
      * updates the element to display and whether there was an exact match.
      */
     let hline = "+" + "-".repeat(4) + "+" + "-".repeat(errorColWidth) + "+" + "-".repeat(8) + "+<br>"
-    response.innerHTML += "Results for " + guess + "<br>" + hline
+    response.innerHTML += "Results for " + guess + "<br>" + hline + "|" + "\xa0".repeat(4) + "|" + " Errors/Warnings" + "\xa0".repeat(errorColWidth - 16) + "| Result |<br>" + hline
     for (let i = 0; i < passwords.length; i++) {
         response.innerHTML += "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |"
         try {
             let transformed = transformations[i](guess)
+            // console.log(i + 1, transformed)
             if (transformed.length != 8) {
-                let message = " Warning: Result has length " + transformed.length.toString(16).toUpperCase() + "."
+                let message = " WARNING: Result has length " + transformed.length.toString(16).toUpperCase() + "."
                 response.innerHTML += message + "\xa0".repeat(errorColWidth - message.length) + "|"
             } else {
                 response.innerHTML += " No errors or warnings :)" + "\xa0".repeat(errorColWidth - 25) + "|"
             }
             let [overlap, remainder] = passwordGuessOverlap(passwords[i], transformed)
             if (!overlap.includes("•") && remainder === "") {
-                response.innerHTML += '<b><u style="color: green;">' + overlap + '</u></b>'
+                response.innerHTML += "<b><u style='color: green;'>" + overlap + '</u></b>'
             } else {
-                response.innerHTML += '<b><u style="color: darkred;">' + overlap + '</u></b>'
+                response.innerHTML += "<b><u style='color: orange;'>" + overlap + '</u></b>'
                 response.innerHTML += remainder
             }
         } catch (error) {
-            response.innerHTML += (error as Error).message + "|" + "█".repeat(8)
+            let m = (error as Error).message
+            response.innerHTML += m + "\xa0".repeat(errorColWidth - m.length) + "|<span style='color: darkred;'>" + "█".repeat(8) + "</span>"
         }
         response.innerHTML += "|<br>" + hline
     }
@@ -124,7 +139,7 @@ function processGuess() {
     } else if (guess.match('(^([A-F]|[0-9])+$)')) {
         displayResults(guess)
     } else {
-        response.innerText = guess + " isn't a number I recognize."
+        response.innerText = guess + " isn't recognized as a number. Please ensure that there are no punctuation symbols or other irrelevant characters in your entry."
     }
     // inputBox.value = '' // This clears the input field upon completion.
 }
