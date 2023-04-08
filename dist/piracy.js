6bcf873b4a5de8906534f6f8f0f7c9c68d070960
AlexDuchnowski <alex_duchnowski@brown.edu>
2023-04-07 21:12:54 -0400

    incorporating testsolve feedback

--- dist/piracy.js
+++ dist/piracy.js
@@ -1,7 +1,7 @@
 "use strict";
-let passwords = [
+let inputs = [
     'A5C1C0DE',
-    '0B51D1A5',
+    'DEC15105',
     'D1DAC1C5',
     '60DD55E5',
     'F0CEFEED',
@@ -16,6 +16,7 @@ let passwords = [
 let container = document.getElementById('response-container');
 let inputBox = document.getElementById('guess');
 let errorColWidth = 60;
+let resultColWidth = 20;
 let response = document.createElement('a');
 let defaultMessage = "Type a number in the field above and press Enter to make a guess.";
 response.innerText = defaultMessage;
@@ -60,19 +61,21 @@ function cutAndSwap(s) {
     return s.slice(cutIndex) + s.slice(0, cutIndex);
 }
 let transformations = [
-    hexFunctionMachine((n) => n * 2),
+    hexFunctionMachine((n) => n / 2),
     (hex) => reverseString(simplifyHexString(hex)),
-    hexFunctionMachine((n) => n / 3),
-    hexFunctionMachine((n) => n / 5),
-    hexFunctionMachine((n) => n + parseInt("11111111", 16)),
     hexFunctionMachine((n) => n * 3),
+    hexFunctionMachine((n) => n * 5),
+    hexFunctionMachine((n) => n + parseInt("11111111", 16)),
+    hexFunctionMachine((n) => n / 3),
     hexFunctionMachine((n) => n - parseInt("22222222", 16)),
     (hex) => peelString(simplifyHexString(hex)),
     (hex) => cutAndSwap(simplifyHexString(hex)),
     hexFunctionMachine((n) => parseInt("FFFFFFFF", 16) - n),
-    hexFunctionMachine((n) => Math.sqrt(n)),
+    hexFunctionMachine((n) => n * n),
     hexFunctionMachine((n) => n + parseInt("12345678", 16)),
 ];
+let passwords = inputs.map((x, i) => transformations[i](x));
+console.log(passwords);
 function passwordGuessOverlap(password, guess) {
     /**
      * Computes and returns the strings to display correctly
@@ -80,7 +83,7 @@ function passwordGuessOverlap(password, guess) {
      */
     let result = "";
     let remainder = "";
-    let limit = (guess.length < 8) ? guess.length : 8;
+    let limit = (guess.length < password.length) ? guess.length : password.length;
     for (let i = 0; i < limit; i++) {
         if (password[i] === guess[i]) {
             result += password[i];
@@ -89,8 +92,8 @@ function passwordGuessOverlap(password, guess) {
             result += "•";
         }
     }
-    if (guess.length < 8) {
-        remainder += "•".repeat(8 - guess.length);
+    if (guess.length < password.length) {
+        remainder += "•".repeat(password.length - guess.length);
     }
     return [result, remainder];
 }
@@ -99,14 +102,14 @@ function displayResults(guess) {
      * Takes the response element and a guessed number (in string form) and
      * updates the element to display and whether there was an exact match.
      */
-    let hline = "+" + "-".repeat(4) + "+" + "-".repeat(errorColWidth) + "+" + "-".repeat(8) + "+<br>";
-    response.innerHTML += "Results for " + simplifyHexString(guess) + "<br>" + hline + "|" + "\xa0".repeat(4) + "|" + " Errors/Warnings" + "\xa0".repeat(errorColWidth - 16) + "| Result |<br>" + hline;
+    let hline = "+" + "-".repeat(4) + "+" + "-".repeat(errorColWidth) + "+" + "-".repeat(resultColWidth) + "+<br>";
+    response.innerHTML += "Results for " + simplifyHexString(guess) + "<br>" + hline + "|" + "\xa0".repeat(4) + "|" + " Errors/Warnings" + "\xa0".repeat(errorColWidth - 16) + "| Result" + "\xa0".repeat(resultColWidth - 7) + "|<br>" + hline;
     for (let i = 0; i < passwords.length; i++) {
         response.innerHTML += "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |";
         try {
             let transformed = transformations[i](guess);
             console.log(i + 1, transformed);
-            if (transformed.length != 8) {
+            if (transformed.length != passwords[i].length) {
                 let message = " WARNING: Result has length " + transformed.length.toString(16).toUpperCase() + ".";
                 response.innerHTML += message + "\xa0".repeat(errorColWidth - message.length) + "|";
             }
@@ -121,10 +124,11 @@ function displayResults(guess) {
                 response.innerHTML += "<b><u style='color: orange;'>" + overlap + '</u></b>';
                 response.innerHTML += remainder;
             }
+            response.innerHTML += "\xa0".repeat(resultColWidth - (overlap.length + remainder.length));
         }
         catch (error) {
             let m = error.message;
-            response.innerHTML += m + "\xa0".repeat(errorColWidth - m.length) + "|<span style='color: darkred;'>" + "█".repeat(8) + "</span>";
+            response.innerHTML += m + "\xa0".repeat(errorColWidth - m.length) + "|<span style='color: darkred;'>" + "█".repeat(resultColWidth) + "</span>";
         }
         response.innerHTML += "|<br>" + hline;
     }
--- src/piracy.ts
+++ src/piracy.ts
@@ -1,6 +1,6 @@
-let passwords = [
+let inputs = [
     'A5C1C0DE',
-    '0B51D1A5',
+    'DEC15105',
     'D1DAC1C5',
     '60DD55E5',
     'F0CEFEED',
@@ -16,6 +16,7 @@ let passwords = [
 let container = document.getElementById('response-container') as HTMLElement
 let inputBox = document.getElementById('guess') as HTMLInputElement
 let errorColWidth = 60
+let resultColWidth = 20
 let response = document.createElement('a')
 let defaultMessage = "Type a number in the field above and press Enter to make a guess."
 response.innerText = defaultMessage
@@ -63,19 +64,22 @@ function cutAndSwap(s: string): string {
 }
 
 let transformations = [
-    hexFunctionMachine((n: number) => n * 2),
+    hexFunctionMachine((n: number) => n / 2),
     (hex: string) => reverseString(simplifyHexString(hex)),
-    hexFunctionMachine((n: number) => n / 3),
-    hexFunctionMachine((n: number) => n / 5),
-    hexFunctionMachine((n: number) => n + parseInt("11111111", 16)),
     hexFunctionMachine((n: number) => n * 3),
+    hexFunctionMachine((n: number) => n * 5),
+    hexFunctionMachine((n: number) => n + parseInt("11111111", 16)),
+    hexFunctionMachine((n: number) => n / 3),
     hexFunctionMachine((n: number) => n - parseInt("22222222", 16)),
     (hex: string) => peelString(simplifyHexString(hex)),
     (hex: string) => cutAndSwap(simplifyHexString(hex)),
     hexFunctionMachine((n: number) => parseInt("FFFFFFFF", 16) - n),
-    hexFunctionMachine((n: number) => Math.sqrt(n)),
+    hexFunctionMachine((n: number) => n * n),
     hexFunctionMachine((n: number) => n + parseInt("12345678", 16)),
 ]
+
+let passwords = inputs.map((x, i) => transformations[i](x))
+console.log(passwords)
 
 function passwordGuessOverlap(password: string, guess: string): [string, string] {
     /**
@@ -84,7 +88,7 @@ function passwordGuessOverlap(password: string, guess: string): [string, string]
      */
     let result = ""
     let remainder = ""
-    let limit = (guess.length < 8) ? guess.length : 8
+    let limit = (guess.length < password.length) ? guess.length : password.length
     for (let i = 0; i < limit; i++) {
         if (password[i] === guess[i]) {
             result += password[i]
@@ -92,8 +96,8 @@ function passwordGuessOverlap(password: string, guess: string): [string, string]
             result += "•"
         }
     }
-    if (guess.length < 8) {
-        remainder += "•".repeat(8 - guess.length)
+    if (guess.length < password.length) {
+        remainder += "•".repeat(password.length - guess.length)
     }
     return [result, remainder]
 }
@@ -103,14 +107,14 @@ function displayResults(guess: string): void {
      * Takes the response element and a guessed number (in string form) and
      * updates the element to display and whether there was an exact match.
      */
-    let hline = "+" + "-".repeat(4) + "+" + "-".repeat(errorColWidth) + "+" + "-".repeat(8) + "+<br>"
-    response.innerHTML += "Results for " + simplifyHexString(guess) + "<br>" + hline + "|" + "\xa0".repeat(4) + "|" + " Errors/Warnings" + "\xa0".repeat(errorColWidth - 16) + "| Result |<br>" + hline
+    let hline = "+" + "-".repeat(4) + "+" + "-".repeat(errorColWidth) + "+" + "-".repeat(resultColWidth) + "+<br>"
+    response.innerHTML += "Results for " + simplifyHexString(guess) + "<br>" + hline + "|" + "\xa0".repeat(4) + "|" + " Errors/Warnings" + "\xa0".repeat(errorColWidth - 16) + "| Result" + "\xa0".repeat(resultColWidth - 7) + "|<br>" + hline
     for (let i = 0; i < passwords.length; i++) {
         response.innerHTML += "| " + (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + " |"
         try {
             let transformed = transformations[i](guess)
             console.log(i + 1, transformed)
-            if (transformed.length != 8) {
+            if (transformed.length != passwords[i].length) {
                 let message = " WARNING: Result has length " + transformed.length.toString(16).toUpperCase() + "."
                 response.innerHTML += message + "\xa0".repeat(errorColWidth - message.length) + "|"
             } else {
@@ -123,9 +127,10 @@ function displayResults(guess: string): void {
                 response.innerHTML += "<b><u style='color: orange;'>" + overlap + '</u></b>'
                 response.innerHTML += remainder
             }
+            response.innerHTML += "\xa0".repeat(resultColWidth - (overlap.length + remainder.length))
         } catch (error) {
             let m = (error as Error).message
-            response.innerHTML += m + "\xa0".repeat(errorColWidth - m.length) + "|<span style='color: darkred;'>" + "█".repeat(8) + "</span>"
+            response.innerHTML += m + "\xa0".repeat(errorColWidth - m.length) + "|<span style='color: darkred;'>" + "█".repeat(resultColWidth) + "</span>"
         }
         response.innerHTML += "|<br>" + hline
     }
